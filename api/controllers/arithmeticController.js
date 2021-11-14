fb240fee8bc471038e437d9c80f8a874566ffd9f
pkhieu <pkhieu1989@gmail.com>
2021-11-14 02:23:50 -0800

    Update arithmeticController.js
    remove incorrect path

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
@@ -28,7 +28,7 @@ exports.calculate = function(req, res) {
   if (! operation) {
     throw new Error("Invalid operation: " + req.query.operation);
   }
-': function(a,b) { return +a + +b },'add': function(a,b) { return +a + +b },
+
   // Validate operands
 
   if (! req.query.operand1 ||
